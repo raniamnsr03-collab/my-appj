2ff953ce97518caea29d7b1a15fa13917ee55f7a
raniamansour <rania.mnsr03@gmail.com>
2025-11-28 20:09:20 +0100

    Initial commit - my-appj

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+
+public class App {
+    public static String greet() {
+        return "Hello DevOps!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+}
