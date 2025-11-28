c0861b44b8078b0d1bdbd8524c50c076df3395fe
raniamansour <rania.mnsr03@gmail.com>
2025-11-28 20:30:39 +0100

    feat: update greeting message in new_feature

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 
 public class App {
     public static String greet() {
-        return "Hello DevOps!";
+        return "Hello DevOps!  rania mansour dsi31";
     }
 
     public static void main(String[] args) {
