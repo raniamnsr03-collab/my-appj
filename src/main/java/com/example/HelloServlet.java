00d8c1dc5c02b95787306c2876d2aad0639039bb
raniamansour <rania.mnsr03@gmail.com>
2025-11-30 21:29:18 +0100

    Convert project to WAR

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,2 +1,16 @@
-package com.example;public class HelloServlet {
+package com.example;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.*;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hello DevOps! Servlet deployed successfully!</h1>");
+    }
 }
